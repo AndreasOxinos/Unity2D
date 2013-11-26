ff18a57e78ad2497c973b442c3ebbe69a4a6a5af
Andreas Oxinos <oxinosa@hotmail.com>
2013-11-26 22:05:29 +0200

    Run and jump animation completed
    Collision between ground and the player for jump and physic

---
+++ 2DPower/Assets/Scripts/Jump.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour
+{
+
+    public string jumpButton = "Fire1";
+    public float jumpPower = 250.0f;
+    public Animator anim;
+    public bool grounded = false;
+    public Transform groundCheck;
+
+    public float minJumpDelay = 0.5f;
+    private float jumpTime = 0.0f;
+    private bool jumped = false;
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
+        jumpTime -= Time.deltaTime;
+        if (Input.GetButtonDown(jumpButton))
+        {
+            if (!jumped)
+            {
+                jumped = true;
+                grounded = false;
+                rigidbody2D.AddForce(transform.up*jumpPower);
+                anim.SetTrigger("Jump");
+                jumpTime = minJumpDelay;
+            }
+        }
+
+        if (grounded && jumpTime <= 0 && jumped)
+        {
+            jumped = false;
+            anim.SetTrigger("Land");
+        }
+    }
+
+
+}
---
+++ 2DPower/Assets/Scripts/MoveLeftRight.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography;
+using UnityEngine;
+using System.Collections;
+
+public class MoveLeftRight : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public string axisName = "Horizontal";
+
+    private Animator anim;
+    
+    void Start ()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+	
+	void Update ()
+	{
+        anim.SetFloat("Speed", Mathf.Abs(Input.GetAxis(axisName)));
+	    if (Input.GetAxis(axisName) < 0)
+	    {
+	        Vector3 newScale = transform.localScale;
+	        newScale.x = -1.0f;
+	        transform.localScale = newScale;
+	    }
+	    else if (Input.GetAxis(axisName) > 0)
+	    {
+            Vector3 newScale = transform.localScale;
+            newScale.x = 1.0f;
+            transform.localScale = newScale;
+	    }
+	    transform.position += transform.right * Input.GetAxis(axisName) * speed * Time.deltaTime;
+	}
+}
+ 
